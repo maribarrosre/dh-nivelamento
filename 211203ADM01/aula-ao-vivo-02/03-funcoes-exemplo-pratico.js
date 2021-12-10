25e7f63006d4ec307e3d26064b1e6c8f70814f1b
Dari Alves <darieltondf@gmail.com>
2021-12-09 23:12:13 -0300

    aula ao vivo 02

--- 211203ADM01/aula-ao-vivo-02/03-funcoes-exemplo-pratico.js
+++ 211203ADM01/aula-ao-vivo-02/03-funcoes-exemplo-pratico.js
@@ -4,59 +4,54 @@ function obterMedia(nota1, nota2, nota3, nota4) {
     let media = somaValores / quantidadeDeProvas;
   
     return media
-}
+  }
   
-  // let provaDeHistoria = 
-  // let provaDeMatematica = 
-  // let provaDeCiencia = 
-  // let provaDeGeografia = 
+  let provaDeHistoria = 5
+  let provaDeMatematica = 9
+  let provaDeCiencia = 8
+  let provaDeGeografia = 7
   
-let mediaNotasProvas = obterMedia(9, 5, 8, 7);
+  let mediaNotasProvas = obterMedia(provaDeHistoria, provaDeMatematica, provaDeCiencia, provaDeGeografia);
   
-console.log("Média:",mediaNotasProvas)
+  console.log("Média:",mediaNotasProvas)
   
-/*
-    Iremos criar uma função que recebe as médias das provas e retorna as seguintes mensagens para cada caso:
-
-    1-Se a média for maior ou igual a 7, deve retornar "Aluno aprovado com mérito!";
-
-    2-Caso contrário, se a média for maior ou igual a 5, deve retornar "Aluno aprovado!"
-
-    3-Caso contrário, deve retornar "Aluno reprovado!"
-*/
+  /**
+   *  Iremos criar uma função que recebe as médias das provas e retorna as seguintes mensagens para cada caso :
+   * 
+   * 1 - Se a média do aluno for maior ou igual a 7, deve retornar "Aluno aprovado com mérito!"
+   * 
+   * 2 - Caso contrário, se a média do aluno for igual ou maior que 5, deve retornar "Aluno aprovado"
+   * 
+   * 3 - Caso contrário, deve retornar "Aluno reprovado"
+   * 
+   */
   
-function mostrarAprovacao(media) {
+  function mostrarAprovacao(media) {
     if(media >= 7) {
-        return "Aluno aprovado com mérito!";
-    } else if (media >= 5) {
-        return "Aluno aprovado!";
+      return "Aluno aprovado com mérito"
+    } else if(media >= 5) {
+      return "Aluno aprovado"
     } else {
-        return "Aluno reprovado!";
+      return "Aluno reprovado"
     }
-}
+  }
   
-let menssagem = mostrarAprovacao(mediaNotasProvas);
-
-console.log(menssagem)
-
-/* 
-    let mediaNotasProvas = obterMedia(5, 5, 8, 7); 
-    obterMedia(5, 5, 0, 7); 
-    obterMedia(9, 5, 8, 7);
-
-*/    
-
-// CÓDIGO COM ERRO NA SEQUÊNCIA LÓGICA
-// function mostrarAprovacao(media) {
-//   if(media >= 5) {
-//     return "Aluno aprovado!";
-//   } else if (media >= 7) {
-//     return "Aluno aprovado com mérito!";
-//   } else {
-//     return "Aluno reprovado!";
-//   }
-// }
-
-// let menssagem = mostrarAprovacao(mediaNotasProvas);
-
-// console.log(menssagem)
+  let menssagem = mostrarAprovacao(mediaNotasProvas);
+  
+  console.log(menssagem)
+  
+  // CÓDIGO COM ERRO NA SEQUÊNCIA LÓGICA
+  // function mostrarAprovacao(media) {
+  //   if(media >= 5) {
+  //     return "Aluno aprovado!";
+  //   } else if (media >= 7) {
+  //     return "Aluno aprovado com mérito!";
+  //   } else {
+  //     return "Aluno reprovado!";
+  //   }
+  // }
+  
+  // let menssagem = mostrarAprovacao(mediaNotasProvas);
+  
+  // console.log(menssagem)
+  
