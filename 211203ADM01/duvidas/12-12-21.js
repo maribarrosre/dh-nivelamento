053a1f4531c95142c4cf7900af6e78ba32ecc36d
Dari Alves <darieltondf@gmail.com>
2021-12-12 21:02:46 -0300

    colearning will

---
+++ 211203ADM01/duvidas/12-12-21.js
@@ -0,0 +1,104 @@
+// // Variáveis
+// let nome = "Will";
+// let numero = 50;
+// // let listaAlunos = new Array('will', 'dari');
+// let listaAlunos = ['will', 'michelli', 'vitao']
+
+// let will = listaAlunos[0]; // índice
+
+
+
+// function validarCPF(cpf) {
+//   if(cpf == 123456) {
+//     return true
+//   } else {
+//     return false
+//   }
+// }
+
+// let validaCPF = validarCPF("123456");
+
+// console.log(validaCPF)
+// let personagens = ['spiderman', 'superman', 'batman']
+// // índice              0            1          2 
+
+// let spiderman = personagens[0]
+// let superman = personagens[1]
+
+
+// function tabuada(numero) {
+//   for(let valor = 1; valor <= 10; valor++) {
+//     console.log(numero + " x " + valor + " = " + numero * valor)
+//   }
+// }
+
+// tabuada(4)
+
+// Manual
+// function somar(valor1, valor2) {
+//   return valor1 + valor2
+// }
+
+// let resultado = somar(100, 50) // 150
+
+// resultado = resultado + 1 // 151
+// resultado++; // 152
+// resultado++; // 153
+// resultado++; // 154
+
+
+// console.log(resultado) // 154
+
+
+
+function somar(valor1, valor2) {
+    return valor1 + valor2
+  }
+  
+  // function multiplicar(valor1, valor2) {
+  //   return valor1 * valor2
+  // }
+  
+  // let numeroA = 10;
+  // let numeroB = 2;
+  
+  // let resultado1 = somar(numeroA, numeroB)
+  // let resultado2 = multiplicar(numeroA, numeroB)
+  
+  // let resultado3 = resultado1 + resultado2
+  
+  // let resultado4 = resultado3 * 5
+  
+  // console.log(resultado4)
+  
+  
+  
+  
+  // function descobrirHeroi(array, heroi) {
+  //   let resultado = array.indexOf(heroi)
+  //   if(resultado > -1) {
+  //     return true
+  //   } else {
+  //     return false
+  //   }
+  // }
+  
+  // let dragonBall = ['Goku', 'Vegeta', 'Kuririn'];
+  // let hero = 'Vegeta';
+  
+  // let resultado2 = descobrirHeroi(dragonBall, hero)
+  
+  // console.log(resultado2)
+  
+  
+  let valor1 = 10;
+  let valor2 = 2;
+  
+  if(valor1 % valor2 != 0) {
+    console.log('ímpar')
+  } else {
+    console.log('par')
+  }
+  
+  
+  
