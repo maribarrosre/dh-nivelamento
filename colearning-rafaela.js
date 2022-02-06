4aaa2b6b41ff5ff51013a1aa974de3193b2eb62f
Dari Alves <darieltondf@gmail.com>
2022-02-06 19:19:36 -0300

    Create colearning-rafaela.js

---
+++ colearning-rafaela.js
@@ -0,0 +1,47 @@
+let nome = "Rafaela"
+
+let idade = 21
+
+if (idade >= 21) {
+  console.log("Olá, " + nome);
+  console.log("Você pode beber!")
+} else if (idade >= 18) {
+  console.log("Olá, " + nome);
+  console.log("Você pode comer!")
+} else {
+  console.log("Olá, " + nome);
+  console.log("Você não pode comer e nem beber!")
+}
+
+// if (condição) {
+//   // Faça algo
+// } else {
+//   // Caso contrário, faça outra coisa
+// }
+
+// if (condição) {
+//   // Se,... Faça algo
+// } else if (condição) {
+//   // Caso contrário, se .... faça algo diferente
+// } else {
+//   // Caso contrário, faça outra coisa
+// }
+
+
+// if (idade >= 18) {
+//   console.log("Olá, " + nome);
+//   console.log("Você passou")
+// } else {
+//   console.log("Você não posso dirigir")
+// }
+
+let ladosIguais = 1;
+
+if (ladosIguais == 3) {
+  console.log('Equilátero')
+} else if (ladosIguais == 2) {
+  console.log('Isósceles')
+} else {
+  console.log('Escaleno')
+}
+
